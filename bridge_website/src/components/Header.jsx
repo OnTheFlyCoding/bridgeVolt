a5453118267934912dad31a9ab0e30fa7dc8293f
Giovanni Gonzalez <createdtyler@gmail.com>
2024-12-31 20:02:24 -0500

    dashboard complete

--- bridge_website/src/components/Header.jsx
+++ bridge_website/src/components/Header.jsx
@@ -1,29 +1,21 @@
 import {Link} from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/about';
-import bVolt from '../assets/BvoltLogo.png';
+import logo from '../assets/BvoltLogo.png'
 function Header(){
     return(
         <>
         <header className='header'>
-            <div className='title'>
-            BridgeVolt Cooperative. E-bikes For All.
-            </div>
+            <h1 className='turret-road-regular'>BridgeVolt Cooperative. E-bikes For All.</h1>
             <div className="logo">
-            <Link to='/'><img src={bVolt} alt="no image yet" /></Link>
+                <Link to='/'><img src={logo} alt="no image yet" /></Link>
             </div>
-            <br />
-            <br />
-            <br />
-            <div className='navy border'>
+            <div className='navigation border'>
                 <Link to='/About'>About</Link>
                 <Link to='/Donate'>Donate</Link>
                 <Link to='/Equity'>Equity</Link>
                 <Link to='/Visit'>Visit</Link>
-                <Link to='/Visit'>Support</Link>
-
             </div>
-
         </header>
         </>
     )
